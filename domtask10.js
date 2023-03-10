f4c9af1144ea045905af05aece781bbf9985e934
vishnuu96 <riyachowdary37@gmail.com>
2023-03-10 15:42:57 +0530

    domtask10.details in object

--- domtask10.js
+++ domtask10.js
@@ -1,8 +1,19 @@
 const form=document.querySelector('#my-form');
 form.addEventListener('submit',(e)=>{
   e.preventDefault();
-  const name=document.querySelector("#name").value
+  
+  const userName=document.querySelector("#name").value
   const email=document.querySelector("#email").value
-  localStorage.setItem('name',name)
-  localStorage.setItem('email',email)
+  // const userName=e.target.name.value;
+  // const email=e.target.email.value
+  
+  // localStorage.setItem('name',userName)
+  // localStorage.setItem('email',email)
+  const personDetails={
+    name:userName,
+    email:email
+  }
+  
+  localStorage.setItem('details',JSON.stringify(personDetails));
+  
 })
